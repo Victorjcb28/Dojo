7ae669090f1af2d9280222a707e25ac8b5a9dd10
Victor Jose Caraballo Bellorin <victorjcb28@gmail.com>
2016-08-29 21:14:53 -0400

    Add files via upload

---
+++ database/migrations/2016_08_28_214925_create_estudiante_representantes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEstudianteRepresentantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estudiante_representantes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('estudiante_id')->unsigned();
+
+            $table->foreign('estudiante_id')->references('id')->on('estudiantes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('estudiante_representantes');
+    }
+}
