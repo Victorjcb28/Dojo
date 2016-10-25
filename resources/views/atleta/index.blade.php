82fd85d483a1ee9bc5c127aba712ccea6c20b5c0
Victor Jose Caraballo Bellorin <victorjcb28@gmail.com>
2016-10-25 19:17:54 -0400

    Add files via upload

--- resources/views/atleta/index.blade.php
+++ resources/views/atleta/index.blade.php
@@ -1,19 +1,54 @@
 @extends('layouts.admin')
 @section('content')
-    <table class="table">
-        <thead>
-        <th>Nombre</th>
-        <th>Apellido</th>
+<div class="row">
+<div class="col-md-9 col-md-offset-1">
 
-        </thead>
-        @foreach($atletas as $atleta)
-            <tbody>
-            <td>{{ $atleta-> name}}</td>
-            <td>{{ $atleta-> apellido}}</td>
-            <td>
+    <div class="panel panel-default">
 
-            </td>
-            </tbody>
-        @endforeach
-    </table>
+        <div class="panel-heading">Atletas</div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>
+                        <a class="btn btn-primary" href="" role="button">
+                            Nuevo Atleta
+                        </a>
+                    </p>
+                    <p>
+                        Hay {{count($atletas)}} atletas
+                    </p>
+
+                </div>
+                <div class="col-md-6">
+                    <form class="navbar-form navbar-left pull-right" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Table -->
+        <table class="table">
+            <thead>
+            <th>Nombre</th>
+            <th>Clave</th>
+
+
+            </thead>
+            @foreach($atletas as $atleta)
+                <tbody>
+                <td>{{ $atleta -> name}}</td>
+                <td>{{ $atleta -> apellido}}</td>
+
+                </tbody>
+            @endforeach
+        </table>
+    </div>
+</div>
+
+</div>
 @endsection
